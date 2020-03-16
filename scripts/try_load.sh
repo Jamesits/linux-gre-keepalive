08294b3c49d454821044ff7769d8677166914850
James Swineson <github@public.swineson.me>
2020-03-16 03:38:48 -0700

    update CI def

--- scripts/try_load.sh
+++ scripts/try_load.sh
@@ -8,7 +8,7 @@ TUNNEL_TYPE=$1
 XDP_EXECUTABLE=$2
 TUNNEL_INTERFACE_NAME=test1
 
-ip link del ${TUNNEL_INTERFACE_NAME}
+ip link del ${TUNNEL_INTERFACE_NAME} || true
 ip tunnel add ${TUNNEL_INTERFACE_NAME} mode ${TUNNEL_TYPE} local 169.254.1.1 remote 169.254.1.2
 ip link set ${TUNNEL_INTERFACE_NAME} up
 ip link set dev ${TUNNEL_INTERFACE_NAME} xdp object "${XDP_EXECUTABLE}"
